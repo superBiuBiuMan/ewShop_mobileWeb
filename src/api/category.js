876c0d67c6ce799c1e7bc74a090d01aa5c2edf17
superBiuBiu <1464815392@qq.com>
2022-06-30 15:43:56 +0800

    分类页面数据交互的完成,computed使用get,set处理

--- src/api/category.js
+++ src/api/category.js
@@ -1,5 +1,15 @@
 import ajax from "./ajax.js";
 
-export function reqCategory(){
+export function reqCategory() {
     return ajax.get("/api/goods");
+}
+/**
+ * 
+ * @param {string} type 排序方式
+ * @param {number} cid 二级分类id
+ * @param {number} page 页码
+ * @returns 查询结果promise
+ */
+export function reqCategoryGoods(type, cid, page=1) {
+    return ajax.get(`/api/goods?${type}=1&category_id=${cid}&page=${page}`);
 }
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -5,41 +5,60 @@
     </Navbar>
     <div class="category-wrapper">
       <!-- 排序分布栏 -->
-      <div class="navbar"></div>
+      <div class="navbar">
+        <van-tabs v-model:active="activeTab" @click-tab="onClickTab">
+          <van-tab title="销售排序" name="sales"></van-tab>
+          <van-tab title="价格排序" name="price"></van-tab>
+          <van-tab title="评论排序" name="comments_count"></van-tab>
+        </van-tabs>
+      </div>
       <!-- 左侧分类列表 -->
       <div class="left-list">
         <div class="left-item-content">
-          <!-- v-model="activeNames" -->
-          <van-collapse v-model="activeName" accordion>
-            <!-- title为一级列表的展示名  -->
-            <van-collapse-item
-              v-for="(firstLevel, index) in categoryList"
-              :key="firstLevel.id"
-              :title="firstLevel.name"
-              :name="firstLevel.name"
-            >
-              <!-- 二级列表 -->
-              <van-sidebar v-model="activeSecond">
+          <van-sidebar v-model="activeSecond">
+            <!-- 当做一个整体 -->
+            <van-collapse v-model="activeName" accordion>
+              <!-- 循环一级菜单 -->
+              <van-collapse-item
+                v-for="item in categoryList"
+                :key="item.id"
+                :title="item.name"
+                :name="item.name"
+              >
+                <!-- 循环二级菜单 -->
                 <van-sidebar-item
-                  v-for="(secondLevel,index) in firstLevel.children"
-                  :key="secondLevel.id"
-                  :title="secondLevel.name"
+                  v-for="sub in item.children"
+                  :title="sub.name"
+                  :key="sub.id"
+                  @click="getGoods(sub.id)"
                 />
-              </van-sidebar>
-            </van-collapse-item>
-          </van-collapse>
+              </van-collapse-item>
+            </van-collapse>
+          </van-sidebar>
         </div>
       </div>
       <!-- 书籍展示 -->
-      <div class="show-list"></div>
+      <div class="show-list">
+        <van-card
+          v-for="(item) in showGood"
+          :key="item.id"
+          :title="item.title"
+          :price="item.price"
+          :origin-price="Math.floor(item.price*0.8)"
+          :num="item.comments_count	"
+          :tag="item.comments_count>=0?'流行':'热销'"
+          :thumb="item.cover_url"
+          :lazy-load="true"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/common/navbar/Navbar";
-import { ref, onMounted } from "vue";
-import { reqCategory } from "@/api/category";
+import { ref, onMounted ,reactive,computed, watch} from "vue";
+import { reqCategory,reqCategoryGoods, } from "@/api/category";
 export default {
   name: "Category",
   components: {
@@ -47,19 +66,73 @@ export default {
   },
   setup() {
     //左分类默认项
-    const activeSecond = ref(0);//二级分类列表目前活动项
-    const activeName = ref();//一级分类列表的活动项记录
+    const activeSecond = ref(0); //二级分类列表目前活动项
+    const activeName = ref(); //一级分类列表的活动项记录
+    const activeTab = ref(0); //nav的选中项记录
+    const categoryList = ref([]); //左侧分类列表
+    const currentType = ref('sales');//当前商品默认展示排序方式
+    const currentCid = ref(0);//当前商品的二级分类id
 
-    const categoryList = ref([]);
+    //展示的数据项
+    const showGood = computed({
+      get(){
+        return goods[currentType.value].list;
+      },
+      set(newValue){
+        goods[currentType.value].list = newValue;
+      }
+    })
+
+    //存储请求获取到的数据
+    const goods = reactive({
+      sales:{page:1,list:[]},
+      price:{page:1,list:[]},
+      comments_count:{page:1,list:[]},
+    })
+    /* nva选项被单击回调,结构出排序类别 */
+    function onClickTab({ name }) {
+      // 原始输出
+      // {name: 'price', title: '价格排序', event: PointerEvent, disabled: false}
+      console.log("nav选项被单击", name);
+      //更改类别
+      currentType.value = name;
+    } 
+
+    /* 监视左侧二级列表和排序方式的变化 */
+    watch([currentType,currentCid],async ()=>{  
+        console.log('监视左侧二级列表或排序方式被改变了');
+        //清空原来数据
+        showGood.value = [];
+        //重新发送请求
+        let result = await reqCategoryGoods(currentType.value,currentCid.value);
+        goods[currentType.value].list = result.goods.data;
+    })
+
+    // 左侧列表里面的二级选项被单击
+    function getGoods(id) {
+      console.log("左侧列表里面的二级选项被单击", id);
+      //更新二级分类id记录值
+      currentCid.value = id;
+    }
+    /* 初始化请求数据函数 */
+    // async function reqInit(){
+      
+    // }
     onMounted(async () => {
       let result = await reqCategory();
       //保存存储数据
       categoryList.value = result.categories;
+      //保存默认数据
+      goods.sales.list = result.goods.data;
     });
     return {
       activeSecond,
       activeName,
       categoryList,
+      activeTab,
+      onClickTab,
+      getGoods,
+      showGood
     };
   },
 };
@@ -70,11 +143,11 @@ export default {
   margin-top: 45px;
   display: flex;
   // 为左侧分类列表的宽度
-  @leftItemWidth: 130px;
+  @leftItemWidth: 110px;
   //nav的宽度
   @navBarHidth: 40px;
   .navbar {
-    background-color: red;
+    // background-color: red;
     float: right;
     position: fixed;
     right: 0;
@@ -94,13 +167,14 @@ export default {
   }
   // 书籍展示
   .show-list {
-    background-color: blue;
     position: absolute;
     right: 0;
     // nav的宽度 + 顶部的商品分类宽度
     top: @navBarHidth+45;
     left: @leftItemWidth;
     height: 100%;
+    box-sizing: border-box;
+    padding: 10px;
   }
 }
 //重新定义侧边导航样式
